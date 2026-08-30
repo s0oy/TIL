961d9c5793cc1b613fa483a982f3d3a8390c541f
kimsohy1 <13yh0os@gmail.com>
2026-08-30 16:29:15 +0900

    feat: D045 풀이

---
+++ TIL/algorithm/paiza/2026-08-30-D045-태풍의간격.py
@@ -0,0 +1,11 @@
+# 문제 : 각 태풍에 대해 직전의 태풍과의 상륙일 간격 일수 출력
+# 접근 : 상륙일-직전일 값 출력
+d_1 = int(input())
+d_2 = int(input())
+d_3 = int(input())
+d_4 = int(input())
+d_5 = int(input())
+print(d_2-d_1)
+print(d_3-d_2)
+print(d_4-d_3)
+print(d_5-d_4)
