bb81d0cf478bc64b582f6b7bbc82086b659dd4ed
kimsohy1 <13yh0os@gmail.com>
2026-08-30 16:39:07 +0900

    feat: D046 풀이

---
+++ TIL/algorithm/paiza/2026-08-30-D046-일주일예정.py
@@ -0,0 +1,13 @@
+# 문제 : 유급 신청하지 않으면 갈 수 없는 일수 출력
+# 접근 : 일수를 입력받고 no일수를 카운트해 출력
+d_1 = input()
+d_2 = input()
+d_3 = input()
+d_4 = input()
+d_5 = input()
+d_6 = input()
+d_7 = input()
+
+days = [d_1, d_2, d_3, d_4, d_5, d_6, d_7]
+
+print(days.count("no"))
