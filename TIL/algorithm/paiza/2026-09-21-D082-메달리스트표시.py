d97dfaa20b11da2ac15691c187f077ed65d50995
kimsohy1 <13yh0os@gmail.com>
2026-09-21 12:59:51 +0900

    feat: D082풀이

---
+++ TIL/algorithm/paiza/2026-09-21-D082-메달리스트표시.py
@@ -0,0 +1,8 @@
+# 문제 : 메달 리스트 국명·선수명으로 출력
+# 접근 : 금 -> a, 은 -> b, 동 -> c
+gold = input()
+silver = input()
+bronze = input()
+print(f"Gold {gold}")
+print(f"Silver {silver}")
+print(f"Bronze {bronze}")
